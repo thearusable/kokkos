299e3ba092954b7508ae515bfacce1f5c5a12d14
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-07-26 17:59:08 +0200

    Remove obsolete code

--- core/perf_test/BenchmarkMain.cpp
+++ core/perf_test/BenchmarkMain.cpp
@@ -46,8 +46,6 @@
 
 #include <Kokkos_Core.hpp>
 
-#include <PerfTest_ViewCopy.hpp>  //REMOVE_ME: comparison with original
-
 std::string custom_context() {
   std::ostringstream msg;
   Kokkos::print_configuration(msg);
@@ -64,8 +62,6 @@ int main(int argc, char** argv) {
   benchmark::AddCustomContext("Kokkos configuration", custom_context());
 
   benchmark::RunSpecifiedBenchmarks();
-  // REMOVE_ME: Run the vanilla test for comparison
-  Test::run_deepcopyview_tests8<Kokkos::LayoutRight, Kokkos::LayoutLeft>(10, 1);
 
   benchmark::Shutdown();
   Kokkos::finalize();
